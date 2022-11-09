76f693135e155fc1b4e720a72b6763498043dc72
Baris Tinaz <baris@enhance.online>
2022-11-10 01:55:38 +0300

    add log to post method

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\Log;
 
 /*
 |--------------------------------------------------------------------------
